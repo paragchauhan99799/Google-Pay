8783ef9391fa29022aacbd1cc8e4fdde56891880
paragchauhan99799 <parag99799@gmail.com>
2019-12-03 17:02:14 +0530

    4 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 
+const baseRequest = {
+  apiVersion: 2,
+  apiVersionMinor: 0
+};
+
+const tokenizationSpecification = {
+  type: 'PAYMENT_GATEWAY',
+  parameters: {
+    'gateway': 'example',
+    'gatewayMerchantId': 'exampleGatewayMerchantId'
+  }
+};
+
+const allowedCardNetworks = ["AMEX", "DISCOVER", "INTERAC", "JCB", "MASTERCARD", "VISA"];
+const allowedCardAuthMethods = ["PAN_ONLY", "CRYPTOGRAM_3DS"];
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -7,5 +23,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
-  
+  paymentsClient: any;
+
+  constructor(){}
+
+  ngOnInit() {
+    this.loadGooglrPay();
+    setTimeout(() => {
+      this.initGoogleInstance();
+    }, 2000);
+  }
+
+  loadGooglrPay(): void {
+    const paySrc = 'https://pay.google.com/gp/p/js/pay.js';
+    let isLoaded = false;
+    let scripts:NodeListOf<Element> = document.getElementsByTagName('script');
+    for(let i=0; i < scripts.length; i++) {
+        if (scripts[i].getAttribute('src') == paySrc) isLoaded = true;
+    }
+    if (isLoaded) return;
+    let stripeNode = document.createElement('script');
+    stripeNode.src = paySrc;
+    stripeNode.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(stripeNode);
+  }
+
+  initGoogleInstance() {
+    this.paymentsClient = new google.payments.api.PaymentsClient({
+    environment: 'TEST'
+  });
+  }
 }
